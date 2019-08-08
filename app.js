6970050d9e100b8c7ff8832635b743de645290f6
amrfahmy <amrfahmy57@gmail.com>
2019-08-09 00:08:58 +0200

    add functionality

--- app.js
+++ app.js
@@ -31,7 +31,19 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+app.get("/", function(req, res) {
+  Post.find({}, function(err, posts) {
 
+    res.render("home", {
+
+      startingContent: homeStartingContent,
+
+      posts: posts
+
+    });
+
+  });
+});
 
 
 app.get("/about", function(req, res) {
@@ -48,4 +60,50 @@ app.get("/contact", function(req, res) {
 
 app.get("/compose", function(req, res) {
   res.render("compose");
+});
+
+app.post("/compose", function(req, res) {
+
+  const post = new Post({
+
+    title: req.body.postTitle,
+
+    content: req.body.postBody
+
+  });
+
+  post.save(function(err) {
+
+    if (!err) {
+
+      res.redirect("/");
+
+    }
+
+  });
+
+
+
+});
+
+app.get("/posts/:postId", function(req, res) {
+  const requestedPostId = req.params.postId;
+  Post.findOne({
+    _id: requestedPostId
+  }, function(err, post) {
+
+    res.render("post", {
+
+      title: post.title,
+
+      content: post.content
+
+    });
+
+  });
+
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
